57a484d2af2b4a9bc7cf7cac279c3b761d175714
Camille Cua <ccua@uoguelph.ca>
2020-05-14 17:11:15 -0400

    reviewing game class

--- src/main/java/adventure/Game.java
+++ src/main/java/adventure/Game.java
@@ -18,6 +18,7 @@ public class Game{
     /* this is the class that runs the game.
     You may need some member variables */
     private Adventure adv = new Adventure();
+    private String inputLine; 
 
     public static void main(String args[]){
 
@@ -34,7 +35,6 @@ public class Game{
 
         /* 3. Parse the file the user specified to create the
         adventure, or load your default adventure*/
-        
 
         // 4. Print the beginning of the adventure
 
@@ -42,7 +42,6 @@ public class Game{
 
         // 6. Get the user input. You'll need a Scanner
         Scanner scnr = new Scanner(System.in);
-        String inputLine;
 
         while (true) {
             System.out.println("Would you like to load a json file? (yes or no)");
@@ -80,8 +79,6 @@ public class Game{
             }
         }
 
-        
-
         /* 7+. Use a game instance method to parse the user
         input to learn what the user wishes to do*/
         if (inputLine == "yes") {
@@ -97,12 +94,13 @@ public class Game{
             theGame.generateAdventure(); //what JSONObject obj???
         }
         
-
         //use a game instance method to execute the users wishes*/
-    
 
         /* if the user doesn't wish to quit,
         repeat the steps above*/
+        if (inputLine == "quit") {
+            System.out.println("You are quitting the game?");
+        }
     }
 
     /* you must have these instance methods and may need more*/
@@ -129,7 +127,7 @@ public class Game{
  
     }
     public Adventure generateAdventure(JSONObject obj) {
-        Adventure adv = new Adventure();
+        Adventure adv = new Adventure(); //FIXME
 
         return adv; 
     }
